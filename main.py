a1c81a94a312e9a3b11cbe1e9442754952e0302b
Donphili <donphilip65@yahoo.com>
2024-02-24 00:38:36 +0100

    Quiz game project

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+app.questions = [
+    {
+        "question": "Where is Mt. Everest Located?",
+        "options": ["a. Nepal", "b. India", "c. Sri Lanka", "d. Japan"],
+        "correct_answer": "a"
+    },
+    {
+        "question": "Which team is the winner of IPL 2021?",
+        "options": ["a. MI", "b. RCB", "c. CSK", "d. KKR"],
+        "correct_answer": "c"
+    },
+    {
+        "question": "Who is the father of Computer?",
+        "options": ["a. Albert Einstein", "b. Phunsukh Wangdu", "c. Elon Musk", "d. Charles Babbage"],
+        "correct_answer": "d"
+    }
+]
 
 @app.route('/')
 def index():
@@ -11,21 +28,4 @@ def get_questions():
     return jsonify(app.questions)
 
 if __name__ == "__main__":
-    app.questions = [
-        {
-            "question": "Where is Mt. Everest Located?",
-            "options": ["a. Nepal", "b. India", "c. Sri Lanka", "d. Japan"],
-            "correct_answer": "a"
-        },
-        {
-            "question": "Which team is the winner of IPL 2021?",
-            "options": ["a. MI", "b. RCB", "c. CSK", "d. KKR"],
-            "correct_answer": "c"
-        },
-        {
-            "question": "Who is the father of Computer?",
-            "options": ["a. Albert Einstein", "b. Phunsukh Wangdu", "c. Elon Musk", "d. Charles Babbage"],
-            "correct_answer": "d"
-        }
-    ]
     app.run(host='0.0.0.0', port=5000, debug=True)
